16fdb476adcbd5a334dc20d756ca0857761f0e77
kirezibana <hortensebana32@gmail.com>
2020-11-16 12:54:05 +0200

    adding file

--- password_locker.py
+++ password_locker.py
@@ -4,14 +4,14 @@ from user_credentials import User, Credential
 
 def create_user(fname,lname,password):
 	'''
-	Function to create a new user account
+	This function will be used to create a new user account
 	'''
 	new_user = User(fname,lname,password)
 	return new_user
 
 def save_user(user):
 	'''
-	Function to save a new user account
+	this function will help us to save a new user account
 	'''
 	User.save_user(user)
 
